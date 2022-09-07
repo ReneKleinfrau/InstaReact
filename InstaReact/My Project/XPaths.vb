11212ac64e81d09aa848be5c1920b9702e80ace4
René <112892967+ReneKleinfrau@users.noreply.github.com>
2022-09-07 11:38:56 +0200

    Updated to 0.0.3a

--- InstaReact/My Project/XPaths.vb
+++ InstaReact/My Project/XPaths.vb
@@ -1,4 +1,4 @@
-﻿Module XPaths
+Module XPaths
     'You have to update this
     Public CommentBox_1_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[3]/div/div/form/textarea"
     Public CommentBox_2_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/textarea"
@@ -6,11 +6,11 @@
     Public CommentButton_1_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[3]/div/div/form/button"
     Public CommentButton_2_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[3]/div/form/button"
 
-    Public FollowButton_1_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[1]/div/header/div[2]/div[1]/div[2]/button"
+    Public FollowButton_1_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[1]/div/header/div[2]/div[1]/div[2]/button" 'Button Class above: <button class="_acan _acao _acas" type="button"><div class="_ab8w  _ab94 _ab99 _ab9h
     Public FollowButton_2_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[1]/div/header/div[2]/div[1]/div[2]/button/div"
     Public FollowButton_3_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[1]/div/header/div[2]/div[1]/div[2]/button/div/div"
 
-    Public LikeButton_1_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button"
+    Public LikeButton_1_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button" 'Button direct: <button class="_abl-" type="button"><div class="_abm0 _abm1"
     Public LikeButton_2_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]"
     Public LikeButton_3_XPath As String = "/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button/div[2]"
 
@@ -18,7 +18,6 @@
     Public LoginPassword_XPath As String = "//*[@id=" & ControlChars.Quote & "loginForm" & ControlChars.Quote & "]/div/div[2]/div/label/input"
     Public LoginButton_XPath As String = "//*[@id=" & ControlChars.Quote & "loginForm" & ControlChars.Quote & "]/div/div[3]/button/div"
 
-    Public FollowButton As String = "//*[contains(text(), 'Folgen')]"
     Public CookieWindow_XPath As String = "/html/body/div[4]/div/div/button[1]"
-    Public Notify_XPath As String = "//button[text()=" & ControlChars.Quote & "Jetzt nicht" & ControlChars.Quote & "]"
+    Public Notify_XPath As String = "/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]"
 End Module
